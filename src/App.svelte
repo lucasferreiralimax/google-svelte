a44b884a72b566ed1fc4e42f4a87af357e8d7693
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-22 03:52:35 -0700

    initial files

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import NavComponent from './components/NavComponent.svelte';
+  import FooterComponent from './components/FooterComponent.svelte';
+  import Home from "./views/Home.svelte";
+	import Login from "./views/Login.svelte";
+	import Config from "./views/Config.svelte";
+	import Notfound from "./views/Notfound.svelte";
+</script>
+
+<Router>
+  <NavComponent />
+  <Route path="/">
+    <Home />
+  </Route>
+  <Route path="login">
+    <Login />
+  </Route>
+  <Route path="config">
+    <Config />
+  </Route>
+  <Route path="*">
+    <Notfound />
+  </Route>
+  <FooterComponent />
+</Router>
