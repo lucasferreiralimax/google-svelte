54db65c637c297fbb5586671740872bfcba75a04
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-22 04:34:07 -0700

    addition SearchComponent and import in Home

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,5 +1,7 @@
 <script>
   import HeaderComponent from '../components/HeaderComponent.svelte';
+  import SearchComponent from '../components/SearchComponent.svelte';
 </script>
 
 <HeaderComponent />
+<SearchComponent />
