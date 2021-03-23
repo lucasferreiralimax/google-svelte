c16dedfad18df25b3c21be31121bb7610a9fe20c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-22 21:38:27 -0700

    adjustment file service and manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -62,7 +62,7 @@ self.addEventListener('fetch', (evt) => {
           .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
-                  return cache.match('offline.html');
+                  return cache.match('index.html');
                 });
           })
   );
