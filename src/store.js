19e29ef7140e30f028e829c7c298803b7b6034e0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-25 22:05:47 -0300

    import component voice and active in search component

--- src/store.js
+++ src/store.js
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store';
 export let search_store = writable('');
 export let darkmode_store = writable(false);
 export let keyboard_store = writable(false);
+export let voice_store = writable(false);
--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -3,9 +3,11 @@
   import SearchComponent from '../components/SearchComponent.svelte';
   import ProfileComponent from '../components/ProfileComponent.svelte';
   import Keyboard from '../components/Keyboard/Keyboard.svelte';
+  import Voice from '../components/Voice.svelte';
 </script>
 
 <ProfileComponent />
 <HeaderComponent />
 <SearchComponent />
 <Keyboard />
+<Voice />
